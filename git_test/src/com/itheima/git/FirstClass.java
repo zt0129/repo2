40ad029cf22adc914967e8e071d8281266b808cf
zhangsan <zhangsan@itcaast.cn>
2018-11-26 20:03:23 +0800

    111

--- git_test/src/com/itheima/git/FirstClass.java
+++ git_test/src/com/itheima/git/FirstClass.java
@@ -6,4 +6,9 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i = 10;
+        i += 10;
+    }
 }
